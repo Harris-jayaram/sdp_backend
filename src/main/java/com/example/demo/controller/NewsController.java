d61ae0af51521189d6f6c66c1801fa3d068ea023
HARRIS JAYARAM R <117694759+Harris-jayaram@users.noreply.github.com>
2024-08-16 09:40:19 +0530

    Update NewsController.java

--- src/main/java/com/example/demo/controller/NewsController.java
+++ src/main/java/com/example/demo/controller/NewsController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/news")
-@CrossOrigin(origins = "http://localhost:3000") // Adjust based on your React app's URL
+// @CrossOrigin(origins = "http://localhost:3000") // Adjust based on your React app's URL
 public class NewsController {
     
     @Autowired
