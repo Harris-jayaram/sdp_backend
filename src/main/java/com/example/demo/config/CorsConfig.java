afe0b0fb9a2dca65fe56def9bfa67c8f914edf92
HARRIS JAYARAM R <117694759+Harris-jayaram@users.noreply.github.com>
2024-08-16 13:28:05 +0530

    Update CorsConfig.java

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Set allowed origins from environment variables
+                        .allowedOrigins("https://jobsearchingshigoto.netlify.app/") // Set allowed origins from environment variables
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
