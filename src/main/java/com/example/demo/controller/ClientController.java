0e7eca35fc5cacedd79e142ddbaf6479f8711b94
HARRIS JAYARAM R <117694759+Harris-jayaram@users.noreply.github.com>
2024-08-16 09:39:46 +0530

    Update ClientController.java

--- src/main/java/com/example/demo/controller/ClientController.java
+++ src/main/java/com/example/demo/controller/ClientController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/clients")
-@CrossOrigin(origins = "http://localhost:3000") // Allow requests from React frontend
+// @CrossOrigin(origins = "http://localhost:3000") // Allow requests from React frontend
 public class ClientController {
 
     @Autowired
