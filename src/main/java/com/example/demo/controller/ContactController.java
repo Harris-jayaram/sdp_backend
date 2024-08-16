333ea5197f4d16b37c08f7ea5c7304beb88486d1
HARRIS JAYARAM R <117694759+Harris-jayaram@users.noreply.github.com>
2024-08-16 09:39:58 +0530

    Update ContactController.java

--- src/main/java/com/example/demo/controller/ContactController.java
+++ src/main/java/com/example/demo/controller/ContactController.java
@@ -10,7 +10,7 @@ import com.example.demo.service.ContactService;
 
 @RestController
 @RequestMapping("/api/contact")
-@CrossOrigin(origins = "http://localhost:3000")
+// @CrossOrigin(origins = "http://localhost:3000")
 public class ContactController {
 
     @Autowired
